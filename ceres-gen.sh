cc47aa35569e382c29617518f9ea1f2b770a174b
kdawson133 <kdawson133@startmail.com>
2026-05-28 15:05:36 +1000

    added

---
+++ ceres-gen.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+ollama pull llama3.2:3b
+ollama pull mistral-nemo
+# ollama pull qwen2.5:14b
+# ollama pull hermes3
+# ollama pull phi4
+# ollama pull deepseek-r1:8b
+ollama pull llava:13b
+# ollama pull gemma:e4b
+
+ollama create axel -f axel.modelfile
+ollama create beau -f beau.modelfile
+# ollama create cody -f cody.modelfile
+# ollama create dean -f dean.modelfile
+# ollama create enzo -f enzo.modelfile
+# ollama create fred -f fred.modelfile
+ollama create gene -f gene.modelfile
+# ollama create hank -f hank.modelfile
