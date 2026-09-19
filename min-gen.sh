ec53675bfc0dd893380f4f1f1bc96ff80efe692b
kdawson133 <kdawson133@startmail.com>
2026-09-19 14:36:14 +1000

    updated

--- min-gen.sh
+++ min-gen.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 ollama pull hf.co/ISTA-DASLab/Qwen3.8-27B-GSQ-RCO-GGUF:IQ3_S
+# Use this for redirect error
+# ollama pull --insecure hf.co/ISTA-DASLab/Qwen3.8-27B-GSQ-RCO-GGUF:IQ3_S
 ollama create igor -f igor.modelfile
